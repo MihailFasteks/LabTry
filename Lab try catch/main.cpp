127460c4e128d718e6a54e5a92fc5af4b178b043
MihailFasteks <fastakovskiy.mihail04@gmail.com>
2023-10-10 11:20:40 +0300

    Initial Commit

---
+++ Lab try catch/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab try catch
+//
+//  Created by Michalis on 10.10.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
